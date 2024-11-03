3aa03121850e7917c8361dca7cf48b6b9bcae411
Kapil Dev <talktokapildev@gmail.com>
2024-11-03 15:43:47 +0000

    initial2

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,9 +1,11 @@
+import { UserDetailContext } from "@/app/_context/UserDetailContext";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 
 function Header() {
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
   return (
     <div className="p-3 px-5 flex justify-between shadow-md">
       <div className="flex gap-3 items-center">
@@ -11,6 +13,10 @@ function Header() {
         <h2 className="font-bold text-xl">AI Short Video</h2>
       </div>
       <div className="flex items-center gap-3">
+        <div className="flex gap-1 items-center">
+          <Image src={"/star.png"} width={20} height={20} alt="star" />
+          <h2>{userDetail?.credits}</h2>
+        </div>
         <Button>Dashboard</Button>
         <UserButton />
       </div>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,128 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <div>
-    <h2>Welcome</h2>
-    <Button variant="outline">Click me</Button>
-   </div>
+    <div>
+      {/* <Header/> */}
+      <div>
+        <div className="p-3 px-5 flex items-center fixed w-full bg-white justify-between shadow-md">
+          <div className="flex gap-3 items-center">
+            <Image src={"/logo.svg"} width={30} height={30} alt="logo" />
+            <h2 className="font-bold text-xl">Ai Short Vid</h2>
+          </div>
+          <div className="flex gap-3 items-center">
+            <Link href={"/dashboard"}>
+              <Button>Dashboard</Button>
+            </Link>
+            <UserButton />
+          </div>
+        </div>
+        <section className=" z-50 pt-10">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              Build Your Short Video{" "}
+              <span className="text-primary">With AI</span>{" "}
+            </h1>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+              EEffortlessly Build AI-Generated Short Videos in Minutes
+            </p>
+            <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+              <a
+                href="/dashboard"
+                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+              >
+                Get Started
+                <svg
+                  className="ml-2 -mr-1 w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="py-8 bg-white z-50 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+          <h2 className="font-bold text-3xl">How it Works?</h2>
+          <h2 className="text-md text-gray-500">
+            Give mock interview in just 3 simplar easy step
+          </h2>
+
+          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <a
+              className="block rounded-xl border bg-white
+       border-gray-200 p-8 shadow-xl transition
+       hover:border-pink-500/10 hover:shadow-pink-500/10"
+              href="#"
+            >
+              {/* <AtomIcon className='h-8 w-8'/> */}
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Select Story Type
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              href="#"
+            >
+              {/* <Edit className='h-8 w-8'/> */}
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Select Images Style{" "}
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              href="#"
+            >
+              {/* <Share2 className='h-8 w-8' /> */}
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Generate Video
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+          </div>
+
+          <div className="mt-12 text-center">
+            <a
+              href="/sign-in"
+              className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
+            >
+              Get Started Today
+            </a>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 }
